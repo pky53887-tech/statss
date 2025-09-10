02f44e0bb6fb3e0835c8039f4cdad479e35c2f93
stat <pkycvj@gmail.com>
2025-09-10 13:25:46 +0900

    temp mod.

--- temp.R
+++ temp.R
@@ -1,18 +1,10 @@
 
-
-
-read.csv("./data/dat1.csv")
-
-dat1 = read.csv("data/dat1.csv", header = TRUE)
-
-
-
-
+dat1 = read.csv("./data/dat1.csv", header = TRUE)
 
 library(ggplot2)
 
 p = ggplot(data = dat1, aes(x = a, y = b)) +
   geom_point()
 p
-ggsave("./plot/ppp.pdf", p)
+ggsave("./plot/acs.pdf", p)
 
