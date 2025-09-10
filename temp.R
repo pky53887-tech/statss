5f7ed829ebbd456cd8bf3dabe14a45cc281ebcee
stat <pkycvj@gmail.com>
2025-09-10 13:20:37 +0900

    initial commit

---
+++ temp.R
@@ -0,0 +1,18 @@
+
+
+
+read.csv("./data/dat1.csv")
+
+dat1 = read.csv("data/dat1.csv", header = TRUE)
+
+
+
+
+
+library(ggplot2)
+
+p = ggplot(data = dat1, aes(x = a, y = b)) +
+  geom_point()
+p
+ggsave("./plot/ppp.pdf", p)
+
